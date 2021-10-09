a9063ea389f185fc6b6af08f18023f15a429c035
Seth Stadick <sstadick@gmail.com>
2021-10-09 09:05:45 -0600

    v0.7.1

--- Formula/crabz.rb
+++ Formula/crabz.rb
@@ -5,13 +5,13 @@ class Crabz< Formula
   version "0.6.3"
 
   if OS.linux?
-    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.6.3/crabz-linux-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.1/crabz-linux-amd64 | shasum -a 256
     url "https://github.com/sstadick/crabz/releases/download/v#{version}/crabz-linux-amd64"
-    sha256 "d25c8854327663793cd9ca62924a8d3745239f5252a1d362d7f3015e6ab758c6"
+    sha256 "ded7657727c126d696c853eed637161b96e129bb4d2d65938f5b81d449137b57"
   elsif OS.mac?
-    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.6.3/crabz-macos-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.1/crabz-macos-amd64 | shasum -a 256
     url "https://github.com/sstadick/crabz/releases/download/v#{version}/crabz-macos-amd64"
-    sha256 "96696ce8451b5b50e83219aff9f709e3203a8fb3b9b26a41dbf5ddc836d74e68"
+    sha256 "f5b7b86f99a74f7ae08ce31fef4e4ca5440295ded339d6048baaf54716e8c281"
   end
 
   def install
