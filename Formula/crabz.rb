c5b9edd52dee5dd9f19da63ad49dbd3a46ed34af
Seth Stadick <sstadick@gmail.com>
2021-10-15 09:43:04 -0600

    v0.7.2

--- Formula/crabz.rb
+++ Formula/crabz.rb
@@ -2,16 +2,16 @@ class Crabz< Formula
   desc "Cross platform, fast, compression and decompression tool."
   homepage "https://github.com/sstadick/crabz"
   license any_of: ["MIT", "Unlicense"]
-  version "0.6.3"
+  version "0.7.2"
 
   if OS.linux?
-    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.1/crabz-linux-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.2/crabz-linux-amd64 | shasum -a 256
     url "https://github.com/sstadick/crabz/releases/download/v#{version}/crabz-linux-amd64"
-    sha256 "ded7657727c126d696c853eed637161b96e129bb4d2d65938f5b81d449137b57"
+    sha256 "b5738440e4c51663c1aa5b1e4c6598109c75ddd2dd24ad9537363e39b7257fdb"
   elsif OS.mac?
-    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.1/crabz-macos-amd64 | shasum -a 256
+    # wget -q -S -O - https://github.com/sstadick/crabz/releases/download/v0.7.2/crabz-macos-amd64 | shasum -a 256
     url "https://github.com/sstadick/crabz/releases/download/v#{version}/crabz-macos-amd64"
-    sha256 "f5b7b86f99a74f7ae08ce31fef4e4ca5440295ded339d6048baaf54716e8c281"
+    sha256 "a3352c6dbd7873f07422094863771e00074b2b3299ca960614224ca03b7ac422"
   end
 
   def install
